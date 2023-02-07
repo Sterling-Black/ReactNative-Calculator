5a74d19e7f5592f4a33aa9ab4e8726b2b28f1c0d
sterling-black <tambongsterling@gmail.com>
2023-02-07 01:08:30 +0100

    calculator ui update

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,52 +1,108 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const Calculator = () => {
-  const [resultText, setResultText] = useState('');
+  const [value, setValue] = useState('');
+  const [previousValue, setPreviousValue] = useState('');
+  const [operator, setOperator] = useState(null);
 
-  const buttons = [
-    ['CLEAR', 'DEL'],
-    ['7', '8', '9', '/'],
-    ['4', '5', '6', '*'],
-    ['1', '2', '3', '-'],
-    ['.', '0', '=', '+'],
-  ];
-
-  const handlePress = (text) => {
-    if (text === 'CLEAR') {
-      setResultText('');
-    } else if (text === 'DEL') {
-      setResultText(resultText.slice(0, -1));
-    } else if (text === '=') {
-      try {
-        setResultText(eval(resultText).toString());
-      } catch (e) {
-        setResultText('Error');
+  const handlePress = (type, val) => {
+    if (type === 'number') {
+      setValue(value + val);
+    } else if (type === 'operator') {
+      setOperator(val);
+      setPreviousValue(value);
+      setValue('');
+    } else if (type === 'clear') {
+      setValue('');
+      setPreviousValue('');
+      setOperator(null);
+    } else if (type === 'calculate') {
+      let result = '';
+      switch (operator) {
+        case '+':
+          result = parseFloat(previousValue) + parseFloat(value);
+          break;
+        case '-':
+          result = parseFloat(previousValue) - parseFloat(value);
+          break;
+        case '*':
+          result = parseFloat(previousValue) * parseFloat(value);
+          break;
+        case '/':
+          result = parseFloat(previousValue) / parseFloat(value);
+          break;
+        default:
+          result = value;
       }
-    } else {
-      setResultText(resultText + text);
+      setValue(result.toString());
+      setPreviousValue('');
+      setOperator(null);
     }
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.resultContainer}>
-        <Text style={styles.resultText}>{resultText}</Text>
+      <View style={styles.outputContainer}>
+        <Text style={styles.output}>{value}</Text>
       </View>
-      <View style={styles.buttonContainer}>
-        {buttons.map((row, index) => (
-          <View key={index} style={styles.row}>
-            {row.map((button, buttonIndex) => (
-              <TouchableOpacity
-                key={buttonIndex}
-                style={styles.button}
-                onPress={() => handlePress(button)}
-              >
-                <Text style={styles.buttonText}>{button}</Text>
-              </TouchableOpacity>
-            ))}
-          </View>
-        ))}
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => handlePress('clear')} style={styles.btn}>
+          <Text style={styles.btnText}>C</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePress('operator', '/')} style={styles.btn}>
+          <Text style={styles.btnText}>/</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePress('operator', '*')} style={styles.btn}>
+          <Text style={styles.btnText}>*</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => handlePress('number', 7)} style={styles.btn}>
+          <Text style={styles.btnText}>7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePress('number', 8)} style={styles.btn}>
+          <Text style={styles.btnText}>8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePress('number', 9)} style={styles.btn}>
+          <Text style={styles.btnText}>9</Text>
+        </TouchableOpacity>
+		        <TouchableOpacity onPress={() => handlePress('operator', '-')} style={styles.btn}>
+          <Text style={styles.btnText}>-</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => handlePress('number', 4)} style={styles.btn}>
+          <Text style={styles.btnText}>4</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePress('number', 5)} style={styles.btn}>
+          <Text style={styles.btnText}>5</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePress('number', 6)} style={styles.btn}>
+          <Text style={styles.btnText}>6</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePress('operator', '+')} style={styles.btn}>
+          <Text style={styles.btnText}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => handlePress('number', 1)} style={styles.btn}>
+          <Text style={styles.btnText}>1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePress('number', 2)} style={styles.btn}>
+          <Text style={styles.btnText}>2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePress('number', 3)} style={styles.btn}>
+          <Text style={styles.btnText}>3</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity onPress={() => handlePress('number', 0)} style={[styles.btn, styles.zeroBtn]}>
+          <Text style={styles.btnText}>0</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handlePress('calculate')} style={styles.btn}>
+          <Text style={styles.btnText}>=</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -55,37 +111,46 @@ const Calculator = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#F5FCFF',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  resultContainer: {
-    flex: 2,
-    backgroundColor: '#3E3E3E',
-    justifyContent: 'center',
+  outputContainer: {
+    backgroundColor: '#EFEFEF',
     alignItems: 'flex-end',
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 20,
+    width: '80%'
   },
-  resultText: {
-    color: 'white',
-    fontSize: 30,
-    padding: 20,
-  },
-  buttonContainer: {
-    flex: 8,
-    backgroundColor: '#F5F5F5',
+  output: {
+    color: '#424242',
+    fontSize: 40
   },
   row: {
-    flex: 1,
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+    marginBottom: 20
   },
-  button: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#333333',
-    margin: 1,
+  btn: {
+    backgroundColor: '#4CAF50',
+    padding: 20,
+    borderRadius: 50,
+    elevation: 5
   },
-  buttonText: {
+  btnText: {
     color: 'white',
-    fontSize: 30,
+    fontSize: 20
   },
+  zeroBtn: {
+    width: 120,
+    alignItems: 'flex-start'
+  }
 });
 
+
 export default Calculator;
+
+ 
+
